1d897dfb21cb7165965e19771948bad265504f77
runkey <csi9509@naver.com>
2024-06-06 15:41:42 +0900

    feat: 컴포넌트 생성

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,21 +5,27 @@ function Header() {
     <h1><a href="/">WEB</a></h1>
   </header>
 }
+function Nav() {
+  return <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+function Article() {
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
 function App() {
   return (
     <div>
       <Header></Header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li> 
-          <li><a href="/read/2">css</a></li> 
-          <li><a href="/read/3">js</a></li> 
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Nav />
+      <Article></Article>
     </div>
   );
 }
