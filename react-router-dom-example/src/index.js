f1008022319b6833f8181d10ff2500059f6a725d
runkey <csi9509@naver.com>
2024-06-12 20:23:26 +0900

    feat: Link 기능 써보기

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import { Link, Routes, BrowserRouter, Route } from 'react-router-dom';
 
 function Home() {
   return (
@@ -36,9 +36,9 @@ function App() {
     <div>
       <h1>React Router DOM Example</h1>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/topics">Topics</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />}></Route>
