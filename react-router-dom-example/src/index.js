ac416a76e07d116b2c0a12e119242580db9d071a
runkey <csi9509@naver.com>
2024-06-12 19:55:35 +0900

    feaet: BrowserRouter 추가

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
 function Home() {
   return (
@@ -44,7 +45,9 @@ function App() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
