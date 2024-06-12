72538247ed9b8de6f22945e79040942566af01f9
runkey <csi9509@naver.com>
2024-06-12 19:50:46 +0900

    feat: 3개의 컴포넌트 기본 틀 모양 잡기

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function App(){
-  return(
-    <div>Hello React Router DOM</div>
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      home...
+    </div>
+  )
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>React Router DOM Example</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
   )
 }
 
