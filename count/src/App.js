d69c919de0aabf5e7e6ba622e7c862d2a3268dcf
runkey <csi9509@naver.com>
2024-07-16 10:18:57 +0900

    feat: useState를 useReducer로 변경

--- count/src/App.js
+++ count/src/App.js
@@ -1,24 +1,35 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  function countReducer(oldCount, action) {
+    if (action === 'UP') {
+      return oldCount + 1;
+    } else if (action === 'DOWN') {
+      return oldCount - 1;
+    } else if (action === 'RESET') {
+      return 0;
+    }
+  }
+
+  const [count, countDispatch] = useReducer(countReducer, 0);
   function down() {
-    setCount(count - 1);
+    countDispatch('DOWN');
   }
 
   function reset() {
-    setCount(0);
+    countDispatch('RESET');
   }
 
-  function up(){
-    setCount(count + 1);
+  function up() {
+    countDispatch('UP');
   }
-  
-  return(
+
+  return (
     <div>
       <input type="button" value="-" onClick={down} />
       <input type="button" value="0" onClick={reset} />
       <input type="button" value="+" onClick={up} />
+      <span>{count}</span>
     </div>
   )
 }
