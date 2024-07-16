46817a457adf3607ac7f02c6afc76e3a77ccd3e1
runkey <csi9509@naver.com>
2024-07-16 10:12:15 +0900

    feat: CRA Count

---
+++ count/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  function down() {
+    setCount(count - 1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  function up(){
+    setCount(count + 1);
+  }
+  
+  return(
+    <div>
+      <input type="button" value="-" onClick={down} />
+      <input type="button" value="0" onClick={reset} />
+      <input type="button" value="+" onClick={up} />
+    </div>
+  )
+}
